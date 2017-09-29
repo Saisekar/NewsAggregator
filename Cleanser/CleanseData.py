eafdf1bc70da01a5ab48d770cb3787affe73d14d
Saisekar <saikrishnan.ted@gmail.com>
2017-09-29 23:38:36 +0530

    Update CleanseData.py

--- Cleanser/CleanseData.py
+++ Cleanser/CleanseData.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 def print_func( headline ):
-    if ((headline.find("http://www.moneycontrol.com/news/business/stocks-business")) != -1):
+    if ((headline.find(<url>)) != -1):
         print (headline)
     return
